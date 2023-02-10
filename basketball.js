90fb5fa2787aa711e143b4d6aa3e8e6d120c573e
SharifHafizi <112721514+SharifHafizi@users.noreply.github.com>
2023-02-10 22:40:53 +0100

    init commit

---
+++ basketball.js
@@ -0,0 +1,18 @@
+let scoreHome = document.getElementById("home");
+let count = 0;
+function increament() {
+  count = count + 1;
+  scoreHome.textContent = count;
+}
+function increament2() {
+  count = count + 2;
+  scoreHome.textContent = count;
+}
+function increament3() {
+  count = count + 3;
+  scoreHome.textContent = count;
+}
+function increament0() {
+  count = 0;
+  scoreHome.textContent = count;
+}
